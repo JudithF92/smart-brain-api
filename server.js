a4876c50f15a8b4faa2e66e6b66366717550330e
Judith Feltl <judith.feltl@live.at>
2021-05-16 14:51:40 +0200

    adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-polished-08913',
-    user : 'postgres',
-    password : 'hashi',
-    database : 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
