f7a686505a9565c532cc0603022539f262d441ce
Judith Feltl <judith.feltl@live.at>
2021-05-16 14:43:19 +0200

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-polished-08913',
     user : 'postgres',
     password : 'hashi',
     database : 'smart-brain'
